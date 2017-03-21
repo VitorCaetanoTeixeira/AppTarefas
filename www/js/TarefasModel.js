a6b08554ccaea600ac49df81ea00d4d8c0a327fd
VitorCaetanoTeixeira <vitor130595@gmail.com>
2017-03-21 02:07:22 -0300

    First Commit
    First Commit - App organizador de tarefas, estudo ionicFramework

---
+++ www/js/TarefasModel.js
@@ -0,0 +1,7 @@
+function getTarefas(){
+	this.itens = [
+	{id:"1",titulo:"Item01",subTitulo:"subt",texto:"This is a Facebook styled Card. The header is created from a Thumbnail List item, the content is from a card-body consisting of an image and paragraph text. The footer consists of tabs, icons aligned left, within the card-footer."},
+	{id:"2",titulo:"Item02",subTitulo:"subt",texto:"This is a Facebook styled Card. The header is created from a Thumbnail List item, the content is from a card-body consisting of an image and paragraph text. The footer consists of tabs, icons aligned left, within the card-footer."},
+	{id:"3",titulo:"Item03",subTitulo:"subt",texto:"This is a Facebook styled Card. The header is created from a Thumbnail List item, the content is from a card-body consisting of an image and paragraph text. The footer consists of tabs, icons aligned left, within the card-footer."}
+	];
+}
