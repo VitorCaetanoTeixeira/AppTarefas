4568d99d3e91d4cee1e3e1069bdb6635ad81644f
VitorCaetanoTeixeira <vitor130595@gmail.com>
2017-03-22 02:44:31 -0300

    Novas Funcionalidades
    Remover Tarefa, Adicionar tarefa, e mudança de layout.

--- www/js/TarefasModel.js
+++ www/js/TarefasModel.js
@@ -1,7 +1,16 @@
 function getTarefas(){
 	this.itens = [
-	{id:"1",titulo:"Item01",subTitulo:"subt",texto:"This is a Facebook styled Card. The header is created from a Thumbnail List item, the content is from a card-body consisting of an image and paragraph text. The footer consists of tabs, icons aligned left, within the card-footer."},
-	{id:"2",titulo:"Item02",subTitulo:"subt",texto:"This is a Facebook styled Card. The header is created from a Thumbnail List item, the content is from a card-body consisting of an image and paragraph text. The footer consists of tabs, icons aligned left, within the card-footer."},
-	{id:"3",titulo:"Item03",subTitulo:"subt",texto:"This is a Facebook styled Card. The header is created from a Thumbnail List item, the content is from a card-body consisting of an image and paragraph text. The footer consists of tabs, icons aligned left, within the card-footer."}
+	{titulo:"Item01",data:"subt",finalizada:false,texto:"This is a Facebook styled Card. The header is created from a Thumbnail List item, the content is from a card-body consisting of an image and paragraph text. The footer consists of tabs, icons aligned left, within the card-footer."},
+	{titulo:"Item02",data:"subt",finalizada:false,texto:"This is a Facebook styled Card. The header is created from a Thumbnail List item, the content is from a card-body consisting of an image and paragraph text. The footer consists of tabs, icons aligned left, within the card-footer."},
+	{titulo:"Item03",data:"subt",finalizada:false,texto:"This is a Facebook styled Card. The header is created from a Thumbnail List item, the content is from a card-body consisting of an image and paragraph text. The footer consists of tabs, icons aligned left, within the card-footer."}
 	];
+
+	this.remove = function(item){
+		var pos = this.itens.indexOf(item);
+		this.itens.splice(pos, 1);
+	};
+
+	this.add = function(item){
+		this.itens.push(item);
+	};
 }
--- www/js/app.js
+++ www/js/app.js
@@ -23,14 +23,73 @@ app.run(function($ionicPlatform) {
   });
 });
 
-app.controller('mainController', function($scope){
+app.controller('mainController', function($scope, $ionicPopup){
 
   var tarefas = new getTarefas();
 
+  
+
   $scope.lista = tarefas.itens; 
+  $scope.showCard = false;
+  $scope.showDelete = false;
 
   $scope.onMarkTask = function(item){
-    console.log("Item id = "+item.id+" Status Tarefa = "+!item.finalizada);
+    //console.log("Item id = "+item.id+" Status Tarefa = "+!item.finalizada);
     item.finalizada = !item.finalizada;
+  };
+
+  $scope.onHideCard = function(item){
+    //console.log("item status = "+ item.finalizada + " scope status = "+$scope.showCard );
+    return item.finalizada && $scope.showCard;
+  };
+
+  $scope.onRemoveCard = function(item){
+    //console.log("item Deletado = "+ item.id);
+    tarefas.remove(item);
+  };
+
+  $scope.onShowDelete = function(){
+    $scope.showDelete = !$scope.showDelete;
+  };
+
+  $scope.onCardAdd = function(){
+    var item = {titulo:"Item04",data:"subt",finalizada:false,texto:"This is a Facebook styled Card. The header is created from a Thumbnail List item, the content is from a card-body consisting of an image and paragraph text. The footer consists of tabs, icons aligned left, within the card-footer."};
+    
+
+    getNewCard(item);
+  };
+
+  //metodos locais
+  function getNewCard(item){
+    $scope.data = {};
+    $scope.data.newTitulo = "";
+    $scope.data.newDate = "";
+    $scope.data.newText = "";
+
+
+    $ionicPopup.show({
+      title: "Nova Tarefa",
+      scope: $scope,
+      template: "<input type='text' placehoder='Titulo da Tarefa' autofocus='true' ng-model='data.newTitulo'>"+
+                "<input type='date' placehoder=''  ng-model='data.newDate'>"+
+                "<textarea  placehoder=''  ng-model='data.newText'>",
+      buttons:[
+        {text:"Confirmar",
+         onTap: function(e){
+          console.log("Evento");
+          item.titulo = $scope.data.newTitulo;
+          console.log($scope.data.newTitulo);
+          item.data = $scope.data.newDate;
+          console.log($scope.data.newDate);
+          item.texto = $scope.data.newText;
+          console.log($scope.data.newText);
+
+          tarefas.add(item);
+
+         }},
+        {text:"Cancelar"}
+        ]
+
+        });
   };
 });
